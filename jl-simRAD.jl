91440135e58a21f6b7e7e9f3fa8d3fad25cda210
Erick Samera <90236200+ericksamera@users.noreply.github.com>
2023-10-29 03:27:20 -0700

    BREAKING!: command-line args + refactor

--- jl-simRAD.jl
+++ jl-simRAD.jl
@@ -1,45 +1,128 @@
 using FASTX
 using Combinatorics
+using Random
+using ArgParse
+using Printf
 
 include("restriction_enzymes.jl")
 
-full_genome = "GCA_940337035.1_PGI_AGRIOTES_LIN_V1_genomic.fna"
-test_seq = "16S.fasta"
-
-size_filter = [300, 600]
-
-for combo in combinations([Enzyme.name for Enzyme in restriction_enzymes_list], 2)
-    println(join(combo, "-"))
+struct FragmentInfo
+    genome_size::Int
+    bases_covered::Int
+    num_fragments::Int
+    total_fragments_before_filtering::Int
 end
 
-# genome_size, bases_covered = open(FASTA.Reader, full_genome) do reader
-#     local_genome_size = 0
-#     local_bases_covered = 0
+function parse_commandline()
+    s = ArgParseSettings()
 
-#     for record in reader
-#         local_genome_size += length(FASTX.sequence(record))
+    @add_arg_table s begin
+        "--min-size", "-m"
+            help = "minimize size of fragments"
+            arg_type = Int
+            default = 300
+        "--max-size", "-M"
+            help = "minimize size of fragments"
+            arg_type = Int
+            default = 600
 
-#         initial_fragment = RestrictionFragment(String(FASTX.sequence(record)), "original", "original")
-#         digest_1 = catalyze(EcoRI, [initial_fragment])
-#         for digest_1_fragments in digest_1
-#             digest_2 = catalyze(NcoI, [digest_1_fragments])
-#             for digest_2_fragment in digest_2
-#                 if digest_2_fragment.left_end == digest_2_fragment.right_end
-#                     continue
-#                 end
+        "--pretty", "-p"
+            help = "minimize size of fragments"
+            arg_type = Bool
+            default = true
+        "--csv"
+            help = "minimize size of fragments"
+            arg_type = Bool
+            default = true
 
-#                 if !(size_filter[1] < length(digest_2_fragment.sequence) < size_filter[2])
-#                     continue
-#                 end
-                
-#                 local_bases_covered += length(digest_2_fragment.sequence)
-#             end
-#         end
-#     end
+        "genome_path"
+            help = "a positional argument"
+            required = true
+        "enzyme_1"
+            help = "a positional argument"
+            required = true
+        "enzyme_2"
+            help = "a positional argument"
+            required = true
+    end
 
-#     return local_genome_size, local_bases_covered
-# end
+    return parse_args(s)
+end
 
-# println(genome_size)
-# println(bases_covered)
-# println(bases_covered/genome_size * 100)
+function digest_sequence(sequence::String, enzyme1::Enzyme, enzyme2::Enzyme, min_size::Int, max_size::Int)
+    initial_fragments = [RestrictionFragment(sequence, "original", "original")]
+    
+    fragments_after_first_digest = catalyze(enzyme1, initial_fragments)
+
+    bases_covered = 0
+    total_fragments = 0
+    num_fragments_after_filter = 0
+
+    for fragment in fragments_after_first_digest
+        fragments_after_second_digest = catalyze(enzyme2, [fragment])
+        
+        for fragment2 in fragments_after_second_digest
+            len = length(fragment2.sequence)
+            total_fragments += 1
+
+            if fragment2.left_end != fragment2.right_end && min_size < len < max_size
+                bases_covered += len
+                num_fragments_after_filter += 1
+            end
+        end
+    end
+    
+    return bases_covered, num_fragments_after_filter, total_fragments
+end
+
+function main()
+
+    parsed_args = parse_commandline()
+
+    if !haskey(enzyme_dict, parsed_args["enzyme_1"])
+        println("Enzyme 1 is not recognized!")
+    end
+
+    if !haskey(enzyme_dict, parsed_args["enzyme_2"])
+        println("Enzyme 2 is not recognized!")
+    end
+
+    info = open(FASTA.Reader, parsed_args["genome_path"]) do reader
+        total_genome_size = 0
+        total_bases_covered = 0
+        total_num_fragments = 0
+        total_fragments_before_filtering = 0
+        
+        for record in reader
+            sequence = String(FASTX.sequence(record))
+            total_genome_size += length(sequence)
+
+            bases_covered, num_fragments, total_fragments = digest_sequence(
+                sequence, 
+                enzyme_dict[parsed_args["enzyme_1"]], 
+                enzyme_dict[parsed_args["enzyme_2"]], 
+                parsed_args["min-size"], 
+                parsed_args["max-size"]
+            )
+
+            total_bases_covered += bases_covered
+            total_num_fragments += num_fragments
+            total_fragments_before_filtering += total_fragments
+        end
+        
+        FragmentInfo(total_genome_size, total_bases_covered, total_num_fragments, total_fragments_before_filtering)
+    end
+    if parsed_args["csv"]
+        @printf("%s-%s,%s,%s,%s,%s\n", parsed_args["enzyme_1"], parsed_args["enzyme_2"], info.bases_covered, info.num_fragments, info.total_fragments_before_filtering, info.bases_covered / info.genome_size * 100)
+    end
+    if parsed_args["pretty"]
+        @printf("# %s-%s\n", parsed_args["enzyme_1"], parsed_args["enzyme_2"])
+        println("# Genome size: ", info.genome_size)
+        println("# Bases covered: ", info.bases_covered)
+        println("# Number of fragments after filtering: ", info.num_fragments)
+        println("# Number of fragments before filtering: ", info.total_fragments_before_filtering)
+        println("# Percentage of bases covered: ", info.bases_covered / info.genome_size * 100, "%")
+    end
+end
+
+main()
